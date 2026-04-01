c294ab06c1f240a42e20a93cfba30215f69689b1
m.bushra9577@gmail.com <m.bushra9577@gmail.com>
2026-04-01 12:11:25 +0400

    Add Brute Force Code

--- BruteForce/Program.cs
+++ BruteForce/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] numbers = {1,3,5,7,9};
+
+int target = 7;
+
+var result = Contains(numbers , target);
+
+Console.WriteLine($"'{target}' {(result ? "Found" : "Not Found")} ");
+
+
+static bool Contains(int[] source , int target) {
+ for (int i = 0 ; i < source.Length ; i ++){
+    if(source[i] == target){
+        return true;
+    }
+ }
+ return false;
+}
