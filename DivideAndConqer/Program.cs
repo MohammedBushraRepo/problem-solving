56b8f340947a438e5230a4489fb8f174a79b3c85
m.bushra9577@gmail.com <m.bushra9577@gmail.com>
2026-04-01 14:19:45 +0400

    using divide and conquer

--- DivideAndConqer/Program.cs
+++ DivideAndConqer/Program.cs
@@ -1,9 +1,10 @@
 ﻿int [] numbers = [2,7,8,18,21,24,28,29,30,33,35,38,40,43,45,50];
 
-int target = 32;
+int target = 50;
 
 
-var index = ContainIterative(numbers , target);
+// var index = ContainIterative(numbers , target);
+var index = ContainsRecursion(numbers , target , 0 , numbers.Length - 1);
 
 Console.WriteLine($"'{target}' {(index != 1 ? $"Found at `{index}`" : "Not Found")} ");
 
@@ -24,4 +25,21 @@ int ContainIterative(int[] source , int target)
           Left = mid + 1;
     }
     return -1; // -1 in programming means not found
+}
+
+
+int ContainsRecursion(int[] source , int target , int left , int right)
+{
+ //Base condition
+ if(left > right)
+  return -1;
+
+ int mid = left + (right - left)/2;
+ if(source[mid] == target)
+   return mid;
+ else if(source[mid] > target)
+   return ContainsRecursion(source , target , left , mid-1);
+ else
+  return ContainsRecursion(source , target , mid + 1 , right);
+
 }
